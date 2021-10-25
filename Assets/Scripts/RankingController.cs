f7ec800ffd804396afcd8f60904c7412c1eea870
sorude26 <sorude26@gmail.com>
2021-10-25 16:33:38 +0900

    ランキング機能を作成した

---
+++ Assets/Scripts/RankingController.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using NCMB;
+using Unity.Collections;
+
+public class RankingController : MonoBehaviour
+{
+    [SerializeField]
+    Text m_rankingNameText = default;
+    [SerializeField]
+    Text m_rankingScoreText = default;
+    [SerializeField]
+    InputField m_nameInput = default;
+    [SerializeField]
+    RectTransform m_entryPanel = default;
+    [SerializeField]
+    float m_gracePeriod = 10f;
+    float m_timer;
+    List<NCMBObject> m_ranking = default;
+    int m_score;
+    bool m_closable = false;
+    bool m_load = false;
+    void Update()
+    {
+        if (!m_closable)
+        {
+            m_timer += Time.deltaTime;
+
+            if (m_timer > m_gracePeriod)
+            {
+                m_closable = true;
+            }
+        }
+    }
+    public void CloseRanking()
+    {
+        if (m_load)
+        {
+            return;
+        }
+        if (m_closable && !m_entryPanel.gameObject.activeSelf) 
+        {
+            m_load = true;
+            m_ranking.Clear();
+            FadeController.Instance.StartFadeOut(GameManager.Instance.LoadScene);
+        }
+    }
+    public void GetRanking(int score)
+    {
+        m_score = score;
+
+        NCMBQuery<NCMBObject> query = new NCMBQuery<NCMBObject>("HighScore");
+        query.OrderByDescending("Score");
+        query.Limit = 7;
+        query.FindAsync((List<NCMBObject> objList, NCMBException e) =>
+        {
+            if (e != null)
+            {
+                Debug.LogError(e.ToString());
+            }
+            else
+            {
+                m_ranking = objList;
+                MakeRankingText();
+
+                if ((score > 0 && m_ranking.Count < 7) || score > int.Parse(m_ranking[m_ranking.Count - 1]["Score"].ToString()) || m_ranking.Count == 0)
+                {
+                    m_entryPanel.gameObject.SetActive(true);
+                }
+            }
+        });
+    }
+    void MakeRankingText()
+    {
+        System.Text.StringBuilder builder = new System.Text.StringBuilder();
+        System.Text.StringBuilder builderScore = new System.Text.StringBuilder();
+        for (int i = 0; i < m_ranking.Count; i++)
+        {
+            builder.Append((i + 1).ToString());
+            builder.Append(" : ");
+            builder.AppendLine(m_ranking[i]["Name"].ToString());
+            builderScore.AppendLine(m_ranking[i]["Score"].ToString());
+        }
+        m_rankingNameText.text = builder.ToString();
+        m_rankingScoreText.text = builderScore.ToString();
+    }
+    public void SetScoreOfCurrentPlay(int score)
+    {
+        GetRanking(score);
+    }
+    public void Entry()
+    {
+        NCMBObject obj = new NCMBObject("HighScore");
+        obj["Name"] = m_nameInput.text;
+        obj["Score"] = m_score;
+
+        obj.SaveAsync((NCMBException e) =>
+        {
+            if (e != null)
+            {
+                Debug.LogError(e.ToString());
+            }
+            else
+            {
+                m_entryPanel.gameObject.SetActive(false);
+                GetRanking(0);
+            }
+        });
+    }
+}
