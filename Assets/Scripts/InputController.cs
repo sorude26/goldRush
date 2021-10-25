c81dde8856f04408a11e480bd292904a5cdd8f12
sorude26 <sorude26@gmail.com>
2021-10-25 16:34:56 +0900

    キー操作で移動可能にした

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -42,27 +42,27 @@ public class InputController : MonoBehaviour, IPointerDownHandler, IPointerUpHan
     }
     public void OnPointerDown(PointerEventData eventData)
     {
-        m_beginPos = eventData.position;
+        //m_beginPos = eventData.position;
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        Vector2 dir = eventData.position - m_beginPos;
-        if (dir.y == 0 || dir.magnitude < m_shotInputRange)
-        {
+        //Vector2 dir = eventData.position - m_beginPos;
+        //if (dir.y == 0 || dir.magnitude < m_shotInputRange)
+        //{
             OnShot?.Invoke();
             m_onShot?.Invoke();
             OnMove?.Invoke(0);
-        }
-        else if (dir.x > 0)
-        {
-            //OnShot?.Invoke();
-            OnMove?.Invoke(1);
-        }
-        else if (dir.x < 0)
-        {
-            //OnShot?.Invoke();
-            OnMove?.Invoke(-1);
-        }
+        //}
+        //else if (dir.x > 0)
+        //{
+        //    //OnShot?.Invoke();
+        //    OnMove?.Invoke(1);
+        //}
+        //else if (dir.x < 0)
+        //{
+        //    //OnShot?.Invoke();
+        //    OnMove?.Invoke(-1);
+        //}
     }
 }
